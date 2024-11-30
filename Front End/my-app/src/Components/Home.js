c9193700b4581aed85327591417326f0531aa0a8
FatehPHD <144256135+FatehPHD@users.noreply.github.com>
2024-11-30 10:37:12 -0700

    added and connected front end homepage to show movies (not completed) and made the search bar (not completed)

--- Front End/my-app/src/Components/Home.js
+++ Front End/my-app/src/Components/Home.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Styles/Home.css";
 
-
 function Home() {
-	return (
-		<div className="Home">
-			<div className="Home-body">
-				<h2>Welcome</h2>
-				<p>
-					This is Home Page 
-				</p>
-			</div>
-		</div>
-	);
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = () => {
+    fetch("http://localhost:8080/Movies")
+      .then((res) => {
+        if (res.ok) {
+          console.log("Fetch Successful");
+          return res.json();
+        } else {
+          console.log("Fetch Unsuccessful");
+        }
+      })
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch((error) => console.log("Error"));
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div className="Home">
+      <div>
+        <h1>{movies[0]?.title || "Loading..."}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- Front End/my-app/src/Components/Tickets.js
+++ Front End/my-app/src/Components/Tickets.js
@@ -3,15 +3,20 @@ import "../Styles/Tickets.css";
 
 
 function Tickets() {
-	return (
-		<div className="Tickets">
-			<div className="Tickets-body">
-				<p>
-					Buy your Tickets
-				</p>
-			</div>
-		</div>
-	);
+    return (
+        <div className="Tickets">
+          <div className="Search-Bar-Container">
+            <h2 className="Search-Bar-Heading">
+              Search:
+              <input
+                type="text"
+                placeholder="Search Movies"
+                className="Search-Input"
+              />
+            </h2>
+          </div>
+        </div>
+      );
 }
 
 export default Tickets;
