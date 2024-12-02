25ded715582c0ceb1939fe6a78886a9bde790b41
HasnainScript <hasnainhaider.developer@gmail.com>
2024-12-01 17:42:10 -0700

    Made a PUT request in the backend, updating ticket by email address

--- Back End/AcmePlex/src/main/java/ca/ucalgary/ensf480/Account/StoreController.java
+++ Back End/AcmePlex/src/main/java/ca/ucalgary/ensf480/Account/StoreController.java
@@ -64,5 +64,11 @@ public class StoreController {
         }
         return user;
     }
+    
+    @PutMapping("/users/tickets/{email}")
+    public String updateSeats(@PathVariable String email,  String tickets) {
+        String updatedTickets = userService.updateTickets(email, tickets);
+        return "Updated Tickets: " + updatedTickets;
+    }
 
     }
